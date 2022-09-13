0802eb0c1ccd79cb7cce763fc35b2ebd8e693a47
Amit2509 <amitsrivastava2509@gmail.com>
2022-09-14 05:02:27 +0530

    Update continuous_range.h

--- continuous_range.h
+++ continuous_range.h
@@ -16,4 +16,4 @@ int CmpFuncForQSort (const void * a, const void * b);
 void printCurrentRanges();
 void separateRanges(int* SortedCurrentValues, int numberOfCurrValues);
 void DetectRanges(int* CurrentValues, int numberOfCurrValues);
-int DetectCurrentRanges(int* DigitalCurrentValues, int numOfDigitalValues, int* AnalogCurrentValues);
+int DetectCurrentRanges(int* DigitalCurrentValues, int numOfDigitalValues, int* AnalogCurrentValues, int SensorID);
