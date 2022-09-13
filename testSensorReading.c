8b311041642abb99e70414be037762fb1e9275f9
Amit2509 <amitsrivastava2509@gmail.com>
2022-09-14 00:58:55 +0530

    Create testSensorReading.c

---
+++ testSensorReading.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+
+void testConverD2A()
+{
+   int currentValue;
+   currentValue = ConvertD2A(1146);
+   assert(currentValue == 3);
+  
+   currentValue = ConvertD2A(4096);
+   assert(currentValue == ERROR_INVALID_VALUE);
+}
