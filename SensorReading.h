fe0627082db97095310a75951fb67fa639377c50
Amit2509 <amitsrivastava2509@gmail.com>
2022-09-14 04:39:28 +0530

    Update SensorReading.h

--- SensorReading.h
+++ SensorReading.h
@@ -1,3 +1,7 @@
 #define ERROR_INVALID_VALUE -99
-int ConvertD2A(int digital);
+#define ERROR_INVALID_SENSOR -100
+#define NUM_OF_SENSORS_AVAILABLE 2
+
+int ConvertD2A_1(int digital);
+int ConvertD2A_2(int digital);
 int ReadCurrentSensor(int* Digital, int numOfDigitalValues, int* Analog);
