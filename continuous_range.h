03452b7f1a41c98c18676d35d296117f03bc3663
Amit2509 <amitsrivastava2509@gmail.com>
2022-09-13 19:01:11 +0530

    Update continuous_range.h
    Adding a struct for ranges

--- continuous_range.h
+++ continuous_range.h
@@ -1,3 +1,14 @@
 #include <stdio.h>
 
+#define MAX_POSSIBLE_RANGES    20
+
+typedef struct
+{
+    int rangeMin[10];
+    int rangeMax[10];
+    int rangeCounts[10];
+    int TotalRanges;
+}rangeT;
+
+rangeT Range;
 void DetectRanges(int* CurrentValues);
