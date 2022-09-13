889d2c1a86c50a00cdcf1698d76583c1a6bd7d1c
Amit2509 <amitsrivastava2509@gmail.com>
2022-09-14 02:48:40 +0530

    Update testSensorReading.c

--- testSensorReading.c
+++ testSensorReading.c
@@ -16,3 +16,18 @@ void testConvertD2A()
    assert(currentValue == ERROR_INVALID_VALUE);
   
 }
+
+
+void testReadCurrentSensor()
+{
+  int CurrentDigital[] = {1228, 1228, 2047, 1638, 4095, 3685, 4096}; /*Total 7 samples. Last one is invalid*/
+  int expectedAnalog[] = {3, 3, 5, 4, 10, 9};                        /*Expecting Total 6 samples.*/
+  int numOfCurrSample = sizeof(CurrentDigital)/sizeof(CurrentDigital[0]);
+  int CurrentAnalog[numOfCurrSample];
+  int numOfAnalogValues = ReadCurrentSensor(CurrentDigital, numOfCurrSample, CurrentAnalog);
+  assert(numOfAnalogValues == 6);
+  for(int i = 0; i < numOfAnalogValues; i++)
+  {
+     assert(CurrentAnalog[i] == expectedAnalog[i]);
+  }  
+}
