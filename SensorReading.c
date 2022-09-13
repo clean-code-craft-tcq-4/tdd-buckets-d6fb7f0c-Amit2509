116fc191de49e095062fcaa93586f78067c45671
Amit2509 <amitsrivastava2509@gmail.com>
2022-09-14 04:40:18 +0530

    Update SensorReading.c

--- SensorReading.c
+++ SensorReading.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include "SensorReading.h"
 
-int ConvertD2A(int digital)
+int ConvertD2A_1(int digital)
 {
   if ((digital < 0) || (digital> 4095))
   {
@@ -15,16 +15,31 @@ int ConvertD2A(int digital)
   }
 }
 
-int ReadCurrentSensor(int* Digital, int numOfDigitalValues, int* Analog)
+int ConvertD2A_2(int digital)
+{
+  if ((digital < 0) || (digital> 1022))
+  {
+    return ERROR_INVALID_VALUE;
+  }
+  else
+  {
+    float analog = (float) ((digital-511)*15)/511;
+    return (int)round(analog);
+  }
+}
+
+static void (*ConverterD2A[])(int) = {ConvertD2A_1, ConvertD2A_2};
+
+int ReadCurrentSensor(int* Digital, int numOfDigitalValues, int* Analog, int sensorID)
 {
   int analog;
   int numOfAnalogValues = 0;
   for(int i = 0; i < numOfDigitalValues; i++)
   {
-    analog = ConvertD2A(Digital[i]);
+    analog = ConvertD2A[sensorID](Digital[i]);
     if(analog != ERROR_INVALID_VALUE)
     {
-      Analog[numOfAnalogValues] = analog;
+      Analog[numOfAnalogValues] = abs(analog);
       numOfAnalogValues++;
     }
   }
