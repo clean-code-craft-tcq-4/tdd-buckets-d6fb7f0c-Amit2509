659ce8d908bbebe7fbc5ab60f753720e08761c98
Amit2509 <amitsrivastava2509@gmail.com>
2022-09-13 22:34:25 +0530

    Update testRange.c
    Adding test cases for each function

--- testRange.c
+++ testRange.c
@@ -2,7 +2,49 @@
 #include <assert.h>
 #include "continuous_range.h"
 
-int main()
+void testCmpFuncForQSort()
+{
+  int result;
+  int a,b;
+  a = 2;
+  b = 3;
+  result = CmpFuncForQSort (&a, &b);
+  assert (result == -1);
+  
+  a = 5;
+  b = 2;
+  result = CmpFuncForQSort (&a, &b);
+  assert (result == 3);
+  
+  a = 4;
+  b = 4;
+  result = CmpFuncForQSort (&a, &b);
+  assert (result == 0);
+}
+
+
+void testseparateRanges()
+{
+  int currentSample[] = {4, 5};
+  int numberOfCurrSample = sizeof(currentSample)/sizeof(currentSample[0]);
+  separateRanges(currentSample, numberOfCurrSample);
+  assert (Range.rangeMin[0] == 4);
+  assert (Range.rangeMax[0] == 5);
+  assert (Range.rangeCounts[0] == 2);
+  assert (Range.TotalRanges == 1);
+  
+  
+  currentSample[] = {-4, 5};
+  numberOfCurrSample = sizeof(currentSample)/sizeof(currentSample[0]);
+  separateRanges(currentSample, numberOfCurrSample);
+  assert (Range.rangeMin[0] == -4);
+  assert (Range.rangeMax[0] == -4);
+  assert (Range.rangeCounts[0] == 1);
+  assert (Range.TotalRanges == 2);
+}
+
+
+void testDetectRanges()
 {
   int currentSample[] = {3, 3, 5, 4, 10, 11, 12};
   int numberOfCurrSample = sizeof(currentSample)/sizeof(currentSample[0]);
@@ -11,5 +53,18 @@ int main()
   assert (Range.rangeMin[0] == 3);
   assert (Range.rangeMax[0] == 5);
   assert (Range.rangeCounts[0] == 4);
+  
+  assert (Range.rangeMin[1] == 10);
+  assert (Range.rangeMax[1] == 12);
+  assert (Range.rangeCounts[1] == 3);
+  
   assert (Range.TotalRanges == 2);
+}
+
+
+int main()
+{
+  testseparateRanges();
+  testDetectRanges();  
+  testCmpFuncForQSort();
 }
